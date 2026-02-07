1bc06eabdf76db2c9f5e84ab37e62413f3904d61
Victor <victorescalonag@gmail.com>
2026-02-07 11:26:09 -0500

    [FEATURE] Se agrega worker

--- app/core/db.py
+++ app/core/db.py
@@ -1,21 +1,3 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
@@ -24,7 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=int(os.getenv("DB_POOL_SIZE", "2")),
+    max_overflow=int(os.getenv("DB_MAX_OVERFLOW", "0")),
+    pool_timeout=int(os.getenv("DB_POOL_TIMEOUT", "30")),
+    pool_recycle=int(os.getenv("DB_POOL_RECYCLE", "1800")),
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
